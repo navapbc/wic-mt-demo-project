6a19221d8df449ac35c15a553964310a6d066e1a
Alsia Plybeah <alsia@navapbc.com>
2022-09-13 14:32:39 -0400

    add main to bootstrap module

---
+++ infra/aws_bootstrap/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "wic-mt"
+}
+
+terraform {
+  required_version = "1.2.0"
+
+  backend "s3" {
+    bucket         = "wic-mt-tf-state"
+    key            = "terraform/bootstrap/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = "true"
+    dynamodb_table = "wic_terraform_locks"
+    profile        = "wic-mt" # may need to rethink this; no profile defaults to env variables
+  }
+}
