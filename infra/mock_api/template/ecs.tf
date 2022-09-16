db3096699119c204ded3dfd5843a7b7a0306461a
Alsia Plybeah <alsia@navapbc.com>
2022-09-16 12:41:46 -0400

    update task definitions with vars and rds job

--- infra/mock_api/template/ecs.tf
+++ infra/mock_api/template/ecs.tf
@@ -84,6 +84,16 @@ resource "aws_security_group" "allow-api-traffic" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+resource "aws_security_group_rule" "allow-api-rds" {
+
+    description = "outbound to rds"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    type        = "egress"
+    security_group_id = "${aws_security_group.allow-api-traffic.id}"
+    source_security_group_id = "${aws_security_group.handle-csv.id}"
+}
 
 # todo: change container def to data block
 # todo: specify security group
@@ -96,7 +106,6 @@ resource "aws_ecs_task_definition" "mock-api-ecs-task-definition" {
   requires_compatibilities = ["FARGATE"]
   memory                   = "1024"
   cpu                      = "512"
-  # task_role_arn            =  aws_iam_role.handle-csv.arn
   execution_role_arn       = "arn:aws:iam::546642427916:role/wic-mt-task-executor"
   container_definitions = jsonencode([
     {
@@ -105,19 +114,11 @@ resource "aws_ecs_task_definition" "mock-api-ecs-task-definition" {
       memory    = 1024
       cpu       = 512
       essential = true
-      command   = ["poetry", "run", "create-eligibility-screener-csv"]
       portMappings = [
         {
           containerPort : 8080
         }
-      ],
-      logConfiguration = {
-        logDriver = "awslogs",
-        options = {
-          "awslogs-group"  = "${data.aws_cloudwatch_log_group.mock_api}"
-          "awslogs-region" = "us-east-1"
-        }
-      }
+      ]
       readonlyRootFilesystem = true
       linuxParameters = {
         capabilities = {
@@ -125,12 +126,30 @@ resource "aws_ecs_task_definition" "mock-api-ecs-task-definition" {
         },
         initProcessEnabled = true
       }
+      environment:[
+        {
+          "name": "POSTGRES_USER"
+          "value": "${data.aws_ssm_parameter.db_username.value}"
+        },
+        {
+          "name": "POSTGRES_PASSWORD"
+          "value": "${data.aws_ssm_parameter.db_pw.value}"
+        },
+        {
+          "name": "POSTGRES_DB"
+          "value": "${data.aws_ssm_parameter.db.value}"
+        },
+        {
+          "name": "DB_HOST"
+          "value": "test-wic-mt.cfsgsglh2mrn.us-east-1.rds.amazonaws.com"
+        }
+      ]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
           "awslogs-group"         = "mock-api",
           "awslogs-region"        = "us-east-1",
-          "awslogs-stream-prefix" = "${var.environment_name}"
+          "awslogs-stream-prefix" = "mock-api"
         }
       }
     }
@@ -148,7 +167,26 @@ resource "aws_ecs_task_definition" "mock-api-ecs-task-definition" {
 # resource "aws_iam_role_policy" "handle-csv" {
   # add s3 perms
 # }
+resource "aws_security_group" "handle-csv"{
+  description = "allows internal connections"
+  name = "csv-handler"
+  vpc_id = module.constants.vpc_id
 
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.allow-api-traffic.id}"]
+  }
+  egress {
+    description      = "allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 resource "aws_ecs_task_definition" "handle-csv" {
   family                   = "${var.environment_name}-csv-handler-definition"
   network_mode             = "awsvpc"
@@ -177,12 +215,30 @@ resource "aws_ecs_task_definition" "handle-csv" {
         },
         initProcessEnabled = true
       }
+      environment:[
+        {
+          "name": "POSTGRES_USER"
+          "value": "${data.aws_ssm_parameter.db_username.value}"
+        },
+        {
+          "name": "POSTGRES_PASSWORD"
+          "value": "${data.aws_ssm_parameter.db_pw.value}"
+        },
+        {
+          "name": "POSTGRES_DB"
+          "value": "${data.aws_ssm_parameter.db.value}"
+        },
+        {
+          "name": "DB_HOST"
+          "value": "test-wic-mt.cfsgsglh2mrn.us-east-1.rds.amazonaws.com"
+        }
+      ]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
           "awslogs-group"         = "mock-api",
           "awslogs-region"        = "us-east-1",
-          "awslogs-stream-prefix" = "${var.environment_name}"
+          "awslogs-stream-prefix" = "csv-handler"
         }
       }
     }
@@ -207,20 +263,32 @@ resource "aws_ecs_service" "handle_csv" {
   force_new_deployment = true
 }
 
-resource "aws_ecs_service" "handle_csv" {
-  name            = "${var.environment_name}-csv-handler"
-  cluster         = aws_ecs_cluster.mock-api-ecs-cluster.id
-  task_definition = aws_ecs_task_definition.handle-csv.arn
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = ["subnet-06b4ec8ff6311f69d"]
-    assign_public_ip = true
-  }
-  desired_count = 1
+# private subnet????
+# connect aws public and private instances 
 
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-  force_new_deployment = true
+# managed in the console!
+data "aws_ssm_parameter" "db_username"{
+  name = "POSTGRES_USER"
+}
+data "aws_ssm_parameter" "db_pw"{
+  name = "POSTGRES_PASSWORD"
+}
+data "aws_ssm_parameter" "db"{
+  name = "POSTGRES_DB"
+}
+data "aws_ssm_parameter" "db_host"{
+  name = "DB_HOST"
+}
+resource "aws_db_instance" "mock_api_db" {
+  identifier           = "${var.environment_name}-wic-mt"
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "13.3"
+  instance_class       = "db.t3.micro" # figure out what to put here
+  db_name              = "main"
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+  apply_immediately    = true
+  vpc_security_group_ids = ["${aws_security_group.handle-csv.id}"]
+  username             = "${data.aws_ssm_parameter.db_username.value}" 
+  password             = "${data.aws_ssm_parameter.db_pw.value}" 
 }
