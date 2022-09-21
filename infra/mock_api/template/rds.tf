0a5de93bfbeb00d4542fc560456aefb2eacabda7
Alsia Plybeah <alsia@navapbc.com>
2022-09-21 13:42:34 -0400

    add random api key

--- infra/mock_api/template/rds.tf
+++ infra/mock_api/template/rds.tf
@@ -3,6 +3,7 @@
 # when 176 is merged:
 # update: common/mock_api_db/POSTGRES_PASSWORD in actual parameter store to prevent conflicts
 # update: what the rds setup considers a password
+# update: add updated auth token to ecs task
 
 resource "random_password" "random_db_password" {
   length           = 48
@@ -15,4 +16,17 @@ resource "aws_ssm_parameter" "random_db_password" {
   name  = "common/mock_api_db/POSTGRES_PASSWORD"
   type  = "SecureString"
   value = random_password.random_db_password.result
+}
+
+# Creates an API key we can use to auth containers with
+resource "random_password" "random_api_key" {
+  length           = 16
+  special          = true
+  min_special      = 6
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+resource "aws_ssm_parameter" "random_api_key" {
+  name  = "common/mock_api_db/API_AUTH_TOKEN"
+  type  = "SecureString"
+  value = random_password.random_api_key.result
 }
