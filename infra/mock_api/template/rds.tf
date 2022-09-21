eb0fb3840be084874816cb4bf64c39e6e44e051f
Alsia Plybeah <alsia@navapbc.com>
2022-09-21 13:38:20 -0400

    create random pw for rds

--- infra/mock_api/template/rds.tf
+++ infra/mock_api/template/rds.tf
@@ -1,6 +1,18 @@
-# rds config goes here
-# need db user and pw
-# need security group
-# use postgres instance
+# generates a secure password randomly
 
-# RDS IAM user
+# when 176 is merged:
+# update: common/mock_api_db/POSTGRES_PASSWORD in actual parameter store to prevent conflicts
+# update: what the rds setup considers a password
+
+resource "random_password" "random_db_password" {
+  length           = 48
+  special          = true
+  min_special      = 6
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "random_db_password" {
+  name  = "common/mock_api_db/POSTGRES_PASSWORD"
+  type  = "SecureString"
+  value = random_password.random_db_password.result
+}
