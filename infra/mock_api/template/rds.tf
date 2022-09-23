a07eb728b841b68e2d7e8472578742a432b67b3e
Alsia Plybeah <alsia@navapbc.com>
2022-09-22 21:14:40 -0400

    WMDP-211 Generate API key (#11)
    Ticket
    https://wicmtdp.atlassian.net/browse/WMDP-211
    
    Changes
    added resources to manage randomly generated mock api key and db password
    Context for reviewers
    db password should be handled more sensitively
    key is necessary for mock api functionality
    Testing
    These values should not:
    
    be shown in terraform output during commands such as apply or plan
    should be shown in the AWS console as a secure string
    Steps to replicate:
    
    authenticate the api with the generated key in parameter store
    use the POST action
    run the generate csv task

--- infra/mock_api/template/rds.tf
+++ infra/mock_api/template/rds.tf
@@ -1,3 +1,35 @@
+# generates a secure password randomly
+
+# when 176 is merged:
+# update: common/mock_api_db/POSTGRES_PASSWORD in actual parameter store to prevent conflicts
+# update: what the rds setup considers a password
+# update: add updated auth token to ecs task
+
+resource "random_password" "random_db_password" {
+  length           = 48
+  special          = true
+  min_special      = 6
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "random_db_password" {
+  name  = "/common/mock_api_db/POSTGRES_PASSWORD"
+  type  = "SecureString"
+  value = random_password.random_db_password.result
+}
+
+# Creates an API key we can use to auth containers with
+resource "random_password" "random_api_key" {
+  length           = 16
+  special          = true
+  min_special      = 6
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+resource "aws_ssm_parameter" "random_api_key" {
+  name  = "/common/mock_api_db/API_AUTH_TOKEN"
+  type  = "SecureString"
+  value = random_password.random_api_key.result
+}
 resource "aws_security_group" "rds" {
   description = "allows connections to RDS"
   name        = "mock-api-${var.environment_name}-rds-instance"
@@ -34,7 +66,8 @@ resource "aws_db_instance" "mock_api_db" {
   apply_immediately               = true
   deletion_protection             = true
   storage_encrypted               = true
+  final_snapshot_identifier       = "${var.environment_name}-final"
   vpc_security_group_ids          = ["${aws_security_group.rds.id}"]
   username                        = data.aws_ssm_parameter.db_username.value
-  password                        = data.aws_ssm_parameter.db_pw.value
+  password                        = aws_ssm_parameter.random_db_password.value
 }
