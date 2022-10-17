9b0763813980a90af2d5053aca8ac5c5955e6409
Alsia Plybeah <alsia@navapbc.com>
2022-10-17 14:24:27 -0400

    format

--- infra/eligibility_screener/template/ecs.tf
+++ infra/eligibility_screener/template/ecs.tf
@@ -16,11 +16,11 @@ resource "aws_security_group" "allow-screener-traffic" {
     cidr_blocks = ["0.0.0.0/8"]
   }
   ingress {
-    description      = "VPC CIDR; allows healthchecks"
-    from_port        = 8080
-    protocol         = "tcp"
-    to_port          = 8080
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "VPC CIDR; allows healthchecks"
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -46,22 +46,22 @@ resource "aws_security_group" "allow-lb-traffic" {
   vpc_id      = module.constants.vpc_id
 
   ingress {
-    description = "HTTP traffic from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "HTTP traffic from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
-    description      = "allow lb traffic"
-    from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
-    security_groups  = [
-        "sg-0c50cf775611d9db2",
-      ]
+    description = "allow lb traffic"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    security_groups = [
+      "sg-0c50cf775611d9db2",
+    ]
   }
   egress {
     description      = "allow all outbound traffic from screener"
@@ -80,10 +80,10 @@ resource "aws_security_group" "allow-lb-traffic" {
 # ---------------------------------------
 
 resource "aws_lb" "eligibility-screener" {
-  name = "${var.environment_name}-screener-lb"
-  internal = false
+  name               = "${var.environment_name}-screener-lb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.allow-lb-traffic.id]
+  security_groups    = [aws_security_group.allow-lb-traffic.id]
   subnets = [
     "subnet-05b0618f4ef1a808c",
     "subnet-06067596a1f981034",
@@ -92,29 +92,29 @@ resource "aws_lb" "eligibility-screener" {
     "subnet-09c317466f27bb9bb",
     "subnet-0ccc97c07aa49a0ae"
   ] # find a way to map all the default ones here; hardcoding for now
-  ip_address_type = "ipv4"
+  ip_address_type        = "ipv4"
   desync_mitigation_mode = "defensive"
 }
 
 # must be ip!!
 resource "aws_lb_target_group" "eligibility-screener" {
-  name = "${var.environment_name}-screener-lb"
-  port = 3000
-  protocol = "HTTP"
+  name        = "${var.environment_name}-screener-lb"
+  port        = 3000
+  protocol    = "HTTP"
   target_type = "ip"
-  vpc_id = module.constants.vpc_id
+  vpc_id      = module.constants.vpc_id
   health_check {
     enabled = true
-    port = 3000
+    port    = 3000
   }
 }
 
 resource "aws_lb_listener" "screener" {
   load_balancer_arn = aws_lb.eligibility-screener.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.eligibility-screener.arn
   }
 }
@@ -149,7 +149,7 @@ resource "aws_ecs_service" "eligibility-screener-ecs-service" {
     target_group_arn = aws_lb_target_group.eligibility-screener.arn
     # target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:546642427916:targetgroup/screener-lb-3000/72e92f65fe721cd1" # hardcoded for test purposes; wic-mt-screener target group
     container_name = "${var.environment_name}-eligibility-screener-container" # from the task definition 
-    container_port = 3000 # from the exposed docker container on the screener
+    container_port = 3000                                                     # from the exposed docker container on the screener
   }
 }
 data "aws_cloudwatch_log_group" "eligibility_screener" {
