6bb45d3b71aabaf1986c05e9e27eed13063ee07d
Alsia Plybeah <alsia@navapbc.com>
2022-10-17 10:38:00 -0400

    migrating sg and load balancer from console

--- infra/eligibility_screener/template/ecs.tf
+++ infra/eligibility_screener/template/ecs.tf
@@ -1,4 +1,8 @@
-# security group for screener
+# ---------------------------------------
+#
+# Security Groups
+#
+# ---------------------------------------
 resource "aws_security_group" "allow-screener-traffic" {
   name        = "allow_screener_traffic"
   description = "This rule blocks all traffic unless it is HTTPS for the eligibility screener"
@@ -11,6 +15,22 @@ resource "aws_security_group" "allow-screener-traffic" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/8"]
   }
+  ingress {
+    description      = "VPC CIDR; allows healthchecks"
+    from_port        = 8080
+    protocol         = "tcp"
+    to_port          = 8080
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  # ingress {
+  #   description      = "allow lb traffic"
+  #   from_port        = 0
+  #   to_port          = 65535
+  #   protocol         = "tcp"
+  #   security_groups  = [
+  #       "sg-0c50cf775611d9db2",
+  #     ]
+  # }
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow traffic from internet"
@@ -28,6 +48,71 @@ resource "aws_security_group" "allow-screener-traffic" {
   }
 }
 
+resource "aws_security_group" "allow-lb-traffic" {
+  name        = "screener_load_balancer_sg"
+  description = "Allows load balancers to communicate with tasks"
+  vpc_id      = module.constants.vpc_id
+
+  ingress {
+    description = "HTTP traffic from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "allow lb traffic"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    security_groups  = [
+        "sg-0c50cf775611d9db2",
+      ]
+  }
+  egress {
+    description      = "allow all outbound traffic from screener"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# ---------------------------------------
+#
+# Load Balancing
+#
+# ---------------------------------------
+
+resource "aws_lb" "eligibility-screener" {
+  name = "${var.environment_name}-screener-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.allow-lb-traffic.id]
+  subnets = [
+    "subnet-05b0618f4ef1a808c",
+    "subnet-06067596a1f981034",
+    "subnet-06b4ec8ff6311f69d",
+    "subnet-08d7f1f9802fd20c4",
+    "subnet-09c317466f27bb9bb",
+    "subnet-0ccc97c07aa49a0ae"
+  ] # find a way to map all the default ones here; hardcoding for now
+  ip_address_type = "ipv4"
+  desync_mitigation_mode = "defensive"
+}
+
+# must be ip!!
+resource "aws_lb_target_group" "eligibility-screener" {
+  
+}
+# ---------------------------------------
+#
+# ECS
+#
+# ---------------------------------------
 resource "aws_ecs_cluster" "eligibility-screener-ecs-cluster" {
   name = var.environment_name
 }
@@ -49,10 +134,17 @@ resource "aws_ecs_service" "eligibility-screener-ecs-service" {
     rollback = true
   }
   force_new_deployment = true
+
+  load_balancer {
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:546642427916:targetgroup/screener-lb-3000/72e92f65fe721cd1" # hardcoded for test purposes; wic-mt-screener target group
+    container_name = "${var.environment_name}-eligibility-screener-container" # from the task definition 
+    container_port = 3000 # from the exposed docker container on the screener
+  }
 }
 data "aws_cloudwatch_log_group" "eligibility_screener" {
   name = "screener"
 }
+
 resource "aws_ecs_task_definition" "eligibility-screener-ecs-task-definition" {
   family                   = "${var.environment_name}-screener-task-definition"
   network_mode             = "awsvpc"
